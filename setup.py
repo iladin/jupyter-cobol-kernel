59b6ce6f7cded0f3579b9e2b5e5c8c8757136530
Daniel Silverman <iladin@gmail.com>
2021-03-18 21:05:51 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='jupyter_cobol_kernel',
+      version='0.1',
+      description='COBOL kernel for Jupyter',
+      author='iladin',
+      author_email='iladin@gmail.com',
+      url='https://github.com/iladin/jupyter-cobol-kernel.git',
+      download_url='https://github.com/iladin/jupyter-cobol-kernel/archive/master.zip',
+      packages=['jupyter_cobol_kernel'],
+      keywords=['jupyter', 'kernel', 'cobol']
+      )

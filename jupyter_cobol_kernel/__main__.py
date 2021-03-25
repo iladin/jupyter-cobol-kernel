03601352777b644da30807053328ec06b507cc3a
Daniel Silverman <iladin@gmail.com>
2021-03-25 00:02:35 -0700

    fix issue with import (python 2 -> 3)

--- jupyter_cobol_kernel/__main__.py
+++ jupyter_cobol_kernel/__main__.py
@@ -1,3 +1,3 @@
 from ipykernel.kernelapp import IPKernelApp
-from .juptyer_cobol_kernel import COBOLKernel
-IPKernelApp.launch_instance(kernel_class=COBOLKernel)
+from . import jupyter_cobol_kernel
+IPKernelApp.launch_instance(kernel_class=jupyter_cobol_kernel.COBOLKernel)
